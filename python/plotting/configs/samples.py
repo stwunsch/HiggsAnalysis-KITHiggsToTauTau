cd78bebb8278557a9a55fea541f73161ca9e8b4b
rfriese <raphael.friese@cern.ch>
2016-07-28 15:27:54 +0200

    fix

--- python/plotting/configs/samples.py
+++ python/plotting/configs/samples.py
@@ -119,7 +119,7 @@ class SamplesBase(object):
 		classified_files = input_file.replace("/*.root", "").split(" ")
 		c_files = " ".join("/storage/jbod/epfeffer/updaterootfiles/" + ifile + "_update.root" for ifile in classified_files)
 		config.setdefault("friend_filenames", []).append(c_files)
-		config.setdefault("friend_treenames", []).append("update "*len(c_files))
+		config.setdefault("friend_treenames", []).append("update "*len(classified_files))
 		config.setdefault("nicks", []).append(nick+nick_suffix)
 		return config
 	
