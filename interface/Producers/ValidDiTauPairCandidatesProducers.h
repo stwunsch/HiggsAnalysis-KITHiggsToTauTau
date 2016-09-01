f2f4b55ab226a70117786fb4d9da6d1deb3e7ff7
Raphael Friese <raphael.friese@cern.ch>
2016-09-01 09:42:17 +0200

    minimizing config

--- interface/Producers/ValidDiTauPairCandidatesProducers.h
+++ interface/Producers/ValidDiTauPairCandidatesProducers.h
@@ -86,6 +86,9 @@ public:
 				if (!settings.GetDiTauPairNoHLT())
 				{
 					std::vector<std::string> commonHltPaths = diTauPair.GetCommonHltPaths(product.m_detailedTriggerMatchedLeptons, settings.GetDiTauPairHltPathsWithoutCommonMatchRequired());
+					std::cout << "next " << std::endl;
+					for(auto a: commonHltPaths)
+						std::cout << a << std::endl;
 					validDiTauPair = validDiTauPair && (commonHltPaths.size() > 0);
 					
 					// pt cuts in case one or more HLT paths are matched
