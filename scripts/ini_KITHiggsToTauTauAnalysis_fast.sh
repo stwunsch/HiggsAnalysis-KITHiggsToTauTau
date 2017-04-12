d6c9e2db9c954250624f5a39fd461425539cd9c6
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2017-04-12 20:23:46 +0200

    a

---
+++ scripts/ini_KITHiggsToTauTauAnalysis_fast.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# source Artus ini script
+source $CMSSW_BASE/src/Artus/Configuration/scripts/ini_ArtusAnalysis.sh
+
+# source Harry ini script
+source $CMSSW_BASE/src/Artus/HarryPlotter/scripts/ini_harry_cmssw.sh
+
+# set the environment
+export KITHIGGSTOTAUTAUPATH=$CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau
